f7c0ca51d4074c4bc6448dd2acc21f9888c8ef35
Arjun Iyer <iyera2@s.dcsdk12.org>
2020-11-29 13:20:06 -0700

    Pushing Ex4

---
+++ answers.md
@@ -0,0 +1,3 @@
+# Excerise 4
+
+1. The structure breaks when you say something like "I don't like you", as Magpie responds with "Why do you don't like me?" which makes little sense. This would be improved by making checks to see if there are modifiers such as "do not" applied to the verb.
--- src/Magpie4.java
+++ src/Magpie4.java
@@ -46,6 +46,8 @@ public class Magpie4 {
 		// Responses which require transformations
 		else if (findKeyword(statement, "I want to", 0) >= 0) {
 			response = transformIWantToStatement(statement);
+		} else if (findKeyword(statement, "I want", 0) >= 0) {
+			response = transformIWantStatement(statement);
 		}
 
 		else {
@@ -55,11 +57,43 @@ public class Magpie4 {
 
 			if (psn >= 0 && findKeyword(statement, "me", psn) >= 0) {
 				response = transformYouMeStatement(statement);
+			}
+			psn = findKeyword(statement, "I", 0);
+			if (psn >= 0 && findKeyword(statement, "you", psn) >= 0) {
+				response = transformYouStatement(statement);
 			} else {
 				response = getRandomResponse();
 			}
+
+
 		}
 		return response;
+	}
+
+	private String transformIWantStatement(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
+
+	private String transformYouStatement(String statement) {
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfI = findKeyword(statement, "I", 0);
+		int psnOfYou = findKeyword(statement, "you", psnOfI + 1);
+
+		String restOfStatement = statement.substring(psnOfI + 1, psnOfYou)
+				.trim();
+		return "Why do you " + restOfStatement + " me?";
 	}
 
 	/**
